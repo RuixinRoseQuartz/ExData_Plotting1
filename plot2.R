39d4a7cb65c64ebd28fe0e6ba392577a9cbe0fdc
RuixinRoseQuartz <ruixin.deng@gmail.com>
2016-08-07 20:59:25 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,8 +3,7 @@ dataName <- sapply(read.table("./Desktop/household_power_consumption.txt", nrow 
 names(data) <- name
 data$DateTime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 
-with(data, plot(DateTime, Global_active_power, type = "n", xlab = "daytime", ylab = "Global Active Power (kilowatts)" , axes = F, frame.plot=TRUE))
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-             , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
-axis(2, yaxs = "r")
+with(data, plot(DateTime, Global_active_power, type = "n", xlab = "daytime", ylab = "Global Active Power (kilowatts)"))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 with(data, points(DateTime, Global_active_power, type = "l"))
