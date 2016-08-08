c6bf62dfc9904eb4c92c2ff77ed4b562df7663e2
RuixinRoseQuartz <ruixin.deng@gmail.com>
2016-08-07 21:03:47 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,9 +4,9 @@ names(data) <- name
 data$DateTime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 
 with(data, plot(DateTime, Sub_metering_1, type = "n", ylab = "Energy sub metering"))
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-             , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 with(data, points(DateTime, Sub_metering_1, type = "l"))
 with(data, points(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(data, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", pch = "¢w", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch = "-", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
