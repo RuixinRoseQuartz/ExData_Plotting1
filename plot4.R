2a0b9cb405eee191930fbb302b514ccdc0cd1d00
RuixinRoseQuartz <ruixin.deng@gmail.com>
2016-08-07 21:05:44 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -5,29 +5,30 @@ data$DateTime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S
 
 par(mfrow = c(2, 2))
 ## graph (1,1)
-with(data, plot(DateTime, Global_active_power, type = "n", xlab = "daytime", ylab = "Global Active Power (kilowatts)" , axes = F, frame.plot=TRUE))
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-               +              , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+with(data, plot(DateTime, Global_active_power, type = "n", xlab = "daytime", ylab = "Global Active Power (kilowatts)" , 
+    axes = F, frame.plot=TRUE))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400+,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 axis(2, yaxs = "r")
 with(data, points(DateTime, Global_active_power, type = "l"))
 
 ## graph (1,2)
 with(data, plot(DateTime, Voltage, type = "n", xlab = "datetime", ylab = "Voltage")
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-             , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 with(data, points(DateTime, Voltage, type = "l"))
 
 ##graph (2,1)
 with(data, plot(DateTime, Sub_metering_1, type = "n", ylab = "Energy sub metering"))
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-             , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 with(data, points(DateTime, Sub_metering_1, type = "l"))
 with(data, points(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(data, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", pch = "¢w", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch = "-", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ##graph (2,2)
 with(data, plot(DateTime, Global_reactive_power, type = "n", xlab = "datetime", ylab = "Global Reactive Power"))
-axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400
-             , as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
+axis(1, at=c(as.numeric(min(data$DateTime)), as.numeric(min(data$DateTime))+86400,
+    as.numeric(min(data$DateTime))+2*86400), labels=c("Thu", "Fri", "Sat"))
 with(data, points(DateTime, Global_reactive_power, type = "l"))
